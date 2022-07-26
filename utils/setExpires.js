b9f67e14d96a50134600bbe653ed7b1d85dd797e
Vacilando <tjfulopp@gmail.com>
2022-07-26 18:03:18 +0200

    Adding a comment about expires in the txt itself

--- utils/setExpires.js
+++ utils/setExpires.js
@@ -1,7 +1,11 @@
-const adaptExpires = (expires) => `Expires: ${expires}`
+const adaptExpires = (
+  expires
+) => `# Date and time after which this data is considered stale
+Expires: ${expires}`;
 
-const setExpires = (content, expires) => expires
+const setExpires = (content, expires) =>
+  expires
     ? content.replace('# Expires #', adaptExpires(expires))
-    : content.replace('# Expires #', '# Expires')
+    : content.replace('# Expires #', '# Expires');
 
-module.exports = setExpires
+module.exports = setExpires;
